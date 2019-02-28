2c6a7b8104f8f059dcf6e1fc5bbe7ee7e3d630b5
luca <lucavhw@gmail.com>
2019-02-28 10:04:39 +0100

    4.4

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
 const readlineSync = require("readline-sync");
 
 let n = new Number(readlineSync.question("Enter a number: "));
+let array = []
 
 function multiRand(n){
-  let array = []
   for (i = 0; i < n; i++) {
   array.push(Math.floor(Math.random() * 11));
 }
   return array;
 }
 
-console.log(multiRand(n));
+console.log("Generated array: " + multiRand(n));
+console.log("Average in array: " + n/array.length);
+console.log("Minimum in array: " + Math.min(...array));
+console.log("Maximum in array: " + Math.max(...array));
