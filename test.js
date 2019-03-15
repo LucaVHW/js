042a77623526fba8f900e1e4f026540bf09e5d91
luca <lucavhw@gmail.com>
2019-03-15 10:24:35 +0100

    7.1

--- test.js
+++ test.js
@@ -1,38 +1,12 @@
-let rectangles = [];
+const readlineSync = require("readline-sync");
 
-class Rectangle {
-  constructor(x, y, width, length) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.length = length;
+let question = readlineSync.question("What is the random number?: ")
+let random =Math.floor(Math.random() * 101);
+
+while (question != random) {
+  if (question < random) {
+    question = readlineSync.question("Too low! ")
+  } else {
+    question = readlineSync.question("Too high! ")
   }
 }
-
-function createRectangles(){
-  for (let i = 0; i <= 1000; ++i) {
-    rectangles[i] = new Rectangle(random(), random(), random(), random());
-  }
-}
-
-function random() {
-  return Math.floor(Math.random() * 101);
-}
-
-createRectangles();
-
-rectangles.forEach(function(element) {
-  console.log(element);
-});
-
-  function collision(rec1, rec2) {
-    if (rec1.x < rec2.x + rec2.width &&
-    rec1.x + rec1.width > rec2.x &&
-    rec1.y < rec1.y + rec2.height &&
-    rec1.y + rec1.height > rec2.y) {
-      return "true";
-    }
-    else {
-      return "false";
-    }
-  }
